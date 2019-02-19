55fb8b6b6dce6d7637c2faa5fe9eac5b36d577c9
Thanakrit Lee <thanakrit.lee.2014@gmail.com>
2019-02-19 17:57:52 +0700

    Implement websocket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,48 +11,73 @@ import About from './pages/about/about';
 class App extends Component {
 
   state = {
-    message: ""
+    helloworld: "",
+    username: "",
+    messages: [],
+    currentMessage: ""
   };
 
   componentDidMount() {
+    var self = this;
+
+    // GET Hello world.
     axios.get('http://localhost:8000/api/hello')
-      .then(res => {
-        console.log(res);
-        const message = res.data.message;
-        this.setState({ message });
-      })
+    .then(res => {
+      const helloworld = res.data.message;
+      this.setState({ helloworld });
+    });
+
+    // GET uuid username.
+    axios.get("http://localhost:8000/api/uuid")
+    .then(res => {
+      const username = res.data.message;
+      this.setState({ username });
+    })
+
+    // Listen to websocket.
+    this.ws = new WebSocket("ws://localhost:8000/api/ws");
+    this.ws.addEventListener("message", (e) => {
+      const msg = JSON.parse(e.data);
+      const message = {username: msg.username, message: msg.message};
+      const messages = this.state.messages;
+      messages.push(message);
+      console.log(messages);
+      this.setState({ messages });
+    });
+    this.toggleSendMessage = this.toggleSendMessage.bind(this);
+    this.messageOnChange = this.messageOnChange.bind(this);
   }
 
-  
+  messageOnChange(message) {
+    message = message.currentTarget.value;
+    const currentMessage = {username: this.state.username, message: message};
+    this.setState({ currentMessage });
+  }
+
+  toggleSendMessage() {
+    this.ws.send(JSON.stringify(this.state.currentMessage));
+    this.setState({ currentMessage: {} });
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-          <div>
-            {this.state.message}
-          </div>
+          {this.state.helloworld}
+          <br/>
+          {this.state.username}
+          <br/>
           <NavLink exact to="/home">Home</NavLink>
           <br/>
           <NavLink exact to="/about">About</NavLink>
           <br/>
-            <div>
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/about" component={About} />
-            </div>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <br/>
+          <input type="text" name="message" onChange={this.messageOnChange}/>
+          <br/>
+          <button onClick={this.toggleSendMessage}>Send</button>
+        </header>
       </div>
     );
   }
--- router/router.go
+++ router/router.go
@@ -1,21 +1,34 @@
 package router
 
 import (
+	"log"
 	"net/http"
 
+	"anon-chat/controllers"
 	u "anon-chat/utils"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
+
+// The connected WebSocket clients.
+var clients = make(map[*websocket.Conn]bool)
+
+// A broadcast channel.
+var broadcast = make(chan Message)
+
+// Configure a WebSocket upgrader.
+var upgrader = websocket.Upgrader{}
+
+// Message - a struct for a chat message.
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
 // GetRouter - return a router with registered routes.
 func GetRouter() *mux.Router {
 	router := mux.NewRouter()
-
-	var HelloWorld = func(w http.ResponseWriter, r *http.Request) {
-		u.Response(w, u.Message("Hello world"))
-		return
-	}
 
 	// Serve static files.
 	router.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./client//build/static"))))
@@ -27,5 +40,64 @@ func GetRouter() *mux.Router {
 
 	router.HandleFunc("/api/hello", HelloWorld).Methods("GET")
 
+	router.HandleFunc("/api/uuid", controllers.GenerateUUID).Methods("GET")
+
+	router.HandleFunc("/api/ws", HandleWebSocketConnection).Methods("GET")
+	go HandleWebSocketMessage()
+
 	return router
+}
+
+// HelloWorld - a controller for returning hello world.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	u.Response(w, u.Message("Hello world"))
+	return
+}
+
+// HandleWebSocketConnection - a controller to upgrade a HTTP connection
+// to a WebSocket connection.
+func HandleWebSocketConnection(w http.ResponseWriter, r *http.Request) {
+	// Upgrade initial GET request to a WebSocket.
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Close the connection when the function returns.
+	// Function returns when there's an error reading the message
+	// from the WebSocket client.
+	defer ws.Close()
+
+	// Register the new WebSocket client.
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as a JSON and map it to a Message go object.
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v\n", err)
+			delete(clients, ws)
+			// Breaks the for loop and returns the function.
+			break
+		}
+		// Send the recieved message to the broadcast channel.
+		broadcast <- msg
+	}
+}
+
+// HandleWebSocketMessage - send message from broadcast to every WebSocket clients.
+func HandleWebSocketMessage() {
+	for {
+		// Get the next mesage from the broadcast channel.
+		msg := <-broadcast
+		// Send the message to every WebSocket client that is currently connected.
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
